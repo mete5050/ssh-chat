cf2a79354feaf64784d18530e9e2299b207384c3
mete5050 <33295066+mete5050@users.noreply.github.com>
2018-05-06 13:30:20 +0300

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -2,12 +2,12 @@ import os
 normal = '\033[0m'
 mor= '\033[35m'
 mavi= '\033[36m'
-pmavi = '\033[96m'#p --> parlak
+pmavi = '\033[96m'
 pyesil = '\033[92m'
 sari= '\033[33m'
 print(pmavi)
-ad = str(input("Bilgisayar adini giriniz: " ))
-ip = str(input("IP adresini giriniz: "))
+ad = str(input("Bilgisayar adini giriniz: " ))#bağlanıcağımız bilgisayarın adını giriyotuz Ör: mete , pi , mypc gibi Not:Bilgisayar adını adresini bağlanıcağımız bilgisayarın terminaline "whoami" yazarak öğrenebilirsiniz
+ip = str(input("IP adresini giriniz: "))#bağlanıcağımız bilgisayarın ip adresini  giriyotuz Ör:192.168.123 , 192.168.1.143 gibi Not:İP adresini bağlanıcağımız bilgisayarın terminaline "ifconfig" yazarak öğrenebilirsiniz
 def read():
     os.system("clear")
     os.system("sudo scp "+ad+"@"+ip+":/home/ssh/message.txt download")
